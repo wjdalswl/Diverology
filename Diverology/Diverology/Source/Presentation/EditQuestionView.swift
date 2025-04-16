3ccb913797d7fcaa511de2f8890589912f711988
jeongminji <parapin2274@gmail.com>
2025-04-16 21:22:51 +0900

    [Design] #13 - 질문 수정 페이지 UI 구현

---
+++ Diverology/Diverology/Source/Presentation/EditQuestionView.swift
@@ -0,0 +1,51 @@
+//
+//  EditQuestionView.swift
+//  Diverology
+//
+//  Created by jeongminji on 4/16/25.
+//
+
+import SwiftUI
+
+struct EditQuestionView: View {
+    @State private var text: String = ""
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .top) {
+                BackgroundImageView()
+                
+                VStack {
+                    CustomNavigationBar(
+                        showBackButton: true,
+                        rightItem: AnyView(
+                            Button{
+                                print("저장 눌림")
+                            } label: {
+                                Text("저장")
+                                    .font(.pretendard(.bold, size: 20))
+                                    .foregroundColor(.blue800)
+                            }
+                        )
+                    )
+                    
+                    TextEditor(text: $text)
+                        .padding(.horizontal, 20)
+                        .foregroundColor(.blue900)
+                        .font(.pretendard(.regular, size: 20))
+                        .lineSpacing(5)
+                        .scrollContentBackground(.hidden)
+                        .background(.clear)
+                }
+                .padding(.top, geometry.safeAreaInsets.top)
+            }
+            .ignoresSafeArea(edges: .all)
+        }
+    }
+}
+
+#Preview {
+    EditQuestionView()
+}
+
+
